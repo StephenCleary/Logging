57f3598c40899bace224df0f76e421480ae017c0
Stephen Cleary <github.ourteddybear@xoxy.net>
2020-10-24 12:20:36 -0400

    Edge case unit tests.

---
+++ test/ExceptionLoggingScopeUnitTests/EdgeCaseUnitTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExceptionLoggingScopeUnitTests.Utility;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Nito.Logging;
+using Xunit;
+
+namespace ExceptionLoggingScopeUnitTests
+{
+    public class EdgeCaseUnitTests
+    {
+        [Fact]
+        public void BeginCapturedExceptionLoggingScopes_WithoutLogger_Throws() => LoggingTestUtility.InitializeLogs((logs, logger) =>
+        {
+            Assert.ThrowsAny<ArgumentException>(() =>
+            {
+                try
+                {
+                    throw new InvalidOperationException();
+                }
+                catch (Exception ex)
+                {
+                    ILogger myLogger = null;
+                    using var disposable = myLogger.BeginCapturedExceptionLoggingScopes(ex);
+                }
+            });
+        });
+
+        [Fact]
+        public void BeginCapturedExceptionLoggingScopes_WithoutInitialization_ReturnsNull()
+        {
+            var services = new ServiceCollection();
+            services.AddLogging();
+            var logs = new InMemoryLoggerProvider();
+            services.AddSingleton<ILoggerProvider>(logs);
+            using var provider = services.BuildServiceProvider();
+
+            var logger = provider.GetRequiredService<ILogger<EdgeCaseUnitTests>>();
+
+            try
+            {
+                throw new InvalidOperationException();
+            }
+            catch (Exception ex)
+            {
+                using var disposable = logger.BeginCapturedExceptionLoggingScopes(ex);
+                Assert.Null(disposable);
+            }
+        }
+    }
+}
